5c72a44025ea0443c69579a0925f18365bf370ab
Jamille Galazi <jamillegalazidf7@gmail.com>
2024-07-10 11:58:53 -0300

    Trabalhando com v-if

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <!-- SCRIPT -->
 <script setup>
 // importação dos componentes
-import Exemplo01 from './components/exemplo01interpolacao.vue';
+// import Exemplo01 from './components/exemplo01interpolacao.vue';
+import Exemplo02 from './components/Exemplo02Condicional.vue';
+
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo01> </Exemplo01>
+  <Exemplo02> </Exemplo02>
 </template>
---
+++ src/components/Exemplo02Condicional.vue
@@ -0,0 +1,11 @@
+<!-- SCRIPT -->
+<script setup>
+
+// variável
+let exibir = true
+</script>
+
+<!-- HTML -->
+<template>
+  <h1 v-if="exibir">Hello World!</h1>
+</template>
