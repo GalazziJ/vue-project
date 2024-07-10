26515a3e7a4c6bfd7ddc1c946f3a18ccf6ebccb0
GalazziJ <jamillegalazidf7@gmail.com>
2024-07-10 18:54:12 -0300

    Iniciando projeto vue

--- src/components/Exemplo02Condicional.vue
+++ src/components/Exemplo02Condicional.vue
@@ -1,11 +1,16 @@
 <!-- SCRIPT -->
 <script setup>
 
-// variável
+// variáveis
+let media = 2
 let exibir = true
 </script>
 
 <!-- HTML -->
 <template>
   <h1 v-if="exibir">Hello World!</h1>
+
+  <p v-if="media >= 7">Aprovado(a)</p>
+  <p v-else-if="media >=5">Em exame</p>
+  <p v-else>Reprovado(a)</p>
 </template>
