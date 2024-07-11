98a1eec2c13ee6ba1d04a6792e3a1a631a041b4f
GalazziJ <jamillegalazidf7@gmail.com>
2024-07-10 21:44:37 -0300

    aprendendo v-model

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@
 // import Exemplo01 from './components/exemplo01interpolacao.vue';
 // import Exemplo02 from './components/Exemplo02Condicional.vue';
 // import Exemplo03 from './components/Exemplo03Laco.vue';
-import Exemplo04 from './components/Exemplo04HTML.vue';
+// import Exemplo04 from './components/Exemplo04HTML.vue';
+import Exemplo05 from './components/Exemplo05Model.vue';
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo04> </Exemplo04> 
+  <Exemplo05> </Exemplo05>
 </template>
---
+++ src/components/Exemplo05Model.vue
@@ -0,0 +1,14 @@
+<!-- SCRIPT -->
+<script setup>
+import { ref } from 'vue';
+
+// variáveis
+let texto = ref(''); // exibir em tempo real (deixa a variável reativa)
+</script>
+
+
+<!-- HTML -->
+<template>
+  <input type="text" v-model="texto">
+  <p>{{ texto }}</p>
+</template>
